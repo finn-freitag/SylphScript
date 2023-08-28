b6c8b1d97db39c98f6ee5a0b8c35c52c250dc66d
Finn Freitag <finn-freitag@web.de>
2023-08-28 17:46:18 +0200

    feat: added readKey()

---
+++ SylphScript/Functions/readKey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SylphScript.Functions
+{
+    public class readKey : IFunction
+    {
+        public IFunction NextFunction { get; set; }
+        public ReferenceName AssignedReturnType { get; set; }
+        public IFunction[] AssignedParameters { get; set; }
+
+        public ReferenceName FullName => "readKey";
+
+        public ArgResPermutation Parameters => ArgResPermutation.Build().Add("null");
+
+        public IFunction GetNewInstance()
+        {
+            return new readKey();
+        }
+
+        public ObjectHolder GetResult(VariableHolder variableHolder)
+        {
+            Console.ReadKey();
+            return ObjectHolder.Null;
+        }
+    }
+}
--- SylphScript/FunctionsRegistry.cs
+++ SylphScript/FunctionsRegistry.cs
@@ -14,6 +14,7 @@ namespace SylphScript
         {
             new print(),
             new readLine(),
+            new readKey(),
         };
     }
 }
