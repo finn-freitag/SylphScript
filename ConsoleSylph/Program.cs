d0f5881a62e71a54c4cd95dd49953d4f0c7227b1
Finn Freitag <finn-freitag@web.de>
2023-08-28 14:35:14 +0200

    Added console application

---
+++ ConsoleSylph/Program.cs
@@ -0,0 +1,28 @@
+﻿using SylphScript;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSylph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 1)
+            {
+                if (File.Exists(args[0]))
+                {
+                    Executor.Execute(Parser.Parse(File.ReadAllText(args[0])));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid parameters: just a path to a *.syl file is required!");
+            }
+        }
+    }
+}
