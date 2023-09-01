5c59abf62888629108be67998df46e8e0b3cb8c5
Finn Freitag <finn-freitag@web.de>
2023-09-01 11:54:12 +0200

    feat: added for loop

---
+++ SylphScript/AdditionalParsers/ForLoop3ParamsParser.cs
@@ -0,0 +1,73 @@
+﻿using SylphScript.Functions;
+using SylphScript.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SylphScript.AdditionalParsers
+{
+    public class ForLoop3ParamsParser : IAdditionalParser
+    {
+        public bool isTypeParser => false;
+
+        public (IFunction Function, bool Success) Parse(ref int index, string code, VariableHolder vHolder)
+        {
+            if (ParserHelper.CheckPos(index, code, "for"))
+            {
+                VariableHolder subHolder = vHolder.GetSubHolder("for");
+                index += 3;
+                ParserHelper.SkipSpace(ref index, code);
+                if (code[index] != '(') return (null, false);
+                index++;
+
+                ParserHelper.SkipSpace(ref index, code);
+                IFunction start = null;
+                if (code[index] != ',' && code[index] != ';')
+                {
+                    start = Parser.Parse(ref index, code, subHolder);
+                    ParserHelper.SkipSpace(ref index, code);
+                    if (code[index] != ',' && code[index] != ';') return (null, false);
+                    else index++;
+                }
+                else index++;
+
+                ParserHelper.SkipSpace(ref index, code);
+                IFunction condition = null;
+                if (code[index] != ',' && code[index] != ';')
+                {
+                    condition = Parser.Parse(ref index, code, subHolder);
+                    ParserHelper.SkipSpace(ref index, code);
+                    if (code[index] != ',' && code[index] != ';') return (null, false);
+                    else index++;
+                }
+                else index++;
+
+                ParserHelper.SkipSpace(ref index, code);
+                IFunction repeating = null;
+                if (code[index] != ')')
+                {
+                    repeating = Parser.Parse(ref index, code, subHolder);
+                    ParserHelper.SkipSpace(ref index, code);
+                    if (code[index] != ')') return (null, false);
+                    else index++;
+                }
+                else index++;
+
+                ParserHelper.SkipSpace(ref index, code);
+                if (index >= code.Length || code[index] != '{') return (null, false);
+                index++;
+                IFunction fulfilled = Parser.Parse(ref index, code, subHolder);
+                ParserHelper.SkipSpace(ref index, code);
+                if (index >= code.Length || code[index] != '}') return (null, false);
+                index++;
+
+                _ForLoop3Params forLoop = new _ForLoop3Params();
+                forLoop.AssignedParameters = new IFunction[] { start, condition, repeating, fulfilled };
+                return (forLoop, true);
+            }
+            else return (null, false);
+        }
+    }
+}
---
+++ SylphScript/Functions/_ForLoop3Params.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SylphScript.Functions
+{
+    public class _ForLoop3Params : IFunction
+    {
+        public IFunction NextFunction { get; set; }
+        public ReferenceName AssignedReturnType { get; set; }
+        public IFunction[] AssignedParameters { get; set; }
+
+        public ReferenceName FullName => "_forloop3params";
+
+        public ArgResPermutation Parameters => ArgResPermutation.Build()
+            .Add("null", "object", "bool", "object", "object")
+            .Add("null", "null", "bool", "object", "object")
+            .Add("null", "object", "null", "object", "object")
+            .Add("null", "object", "bool", "null", "object")
+            .Add("null", "object", "null", "null", "object")
+            .Add("null", "null", "null", "object", "object")
+            .Add("null", "null", "bool", "null", "object")
+            .Add("null", "null", "null", "null", "object");
+
+        public IFunction GetNewInstance()
+        {
+            return new _ForLoop3Params();
+        }
+
+        public ObjectHolder GetResult(VariableHolder variableHolder)
+        {
+            VariableHolder subHolder = variableHolder.GetSubHolder("for");
+            if (AssignedParameters[0] != null) AssignedParameters[1].GetResult(subHolder);
+            while(getConditionResult(subHolder))
+            {
+                AssignedParameters[3].GetResult(subHolder);
+                if (AssignedParameters[2] != null) AssignedParameters[2].GetResult(subHolder);
+            }
+            return ObjectHolder.Null;
+        }
+
+        private bool getConditionResult(VariableHolder subHolder)
+        {
+            if (AssignedParameters[1] == null) return true;
+            else return (bool)AssignedParameters[1].GetResult(subHolder).Object;
+        }
+    }
+}
