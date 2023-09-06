854870ff424c3a060373ea12dd1b27eceb2bcc7c
Finn Freitag <finn-freitag@web.de>
2023-09-06 15:05:35 +0200

    docs: changed enum documentation

--- SylphScript/OperatorGrade.cs
+++ SylphScript/OperatorGrade.cs
@@ -9,15 +9,15 @@ namespace SylphScript
     public enum OperatorGrade : byte
     {
         /// <summary>
-        /// For example a boolean comparison.
+        /// For example a multiplication, division or a logic operation.
         /// </summary>
         First = 1,
         /// <summary>
-        /// For example a multiplication, division or a logic operation.
+        /// For example a addition, subtraction or string operations.
         /// </summary>
         Second = 2,
         /// <summary>
-        /// For example a addition, subtraction or string operations.
+        /// For example a boolean comparison.
         /// </summary>
         Third = 3
     }
